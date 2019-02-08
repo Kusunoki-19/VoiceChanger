c45951f3667b1eddd85b3c39976de4b5100b7a8d
Asck <asck.bbc@gmail.com>
2019-02-08 15:56:54 +0900

    リングQへの変更とqueueモジュールの削除、inVoiceQ->inQ, outVoiceQ->outQに変更、それに伴う関数名の変更

--- VoiceChanger/Changer.py
+++ VoiceChanger/Changer.py
@@ -1,11 +1,9 @@
 import threading
-import queue
 import sys
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
-from test.test_buffer import numpy_array
 
 class Changer():
 
@@ -13,75 +11,94 @@ class Changer():
     F_s = 1 / T_s # サンプリング周波数[s^-1]
     N = 4000 #fftごとのデータ数
     T_fft = T_s * N #fft を行う周期[s]
+    Q_LEN = N * 4
 
     CHANNEL = 1 # チャンネル数（1固定）
 
-    inVoiceQ = queue.Queue()
-    outVoiceQ = queue.Queue()
+    #リングQの定義。inQはマイク入力、outQは出力
+    inQ = np.zeros(Q_LEN)
+    outQ = np.zeros(Q_LEN)
+    #各キューのfrontとrear -> F と R
+    inQF = 2 * N
+    inQR = 0
+    outQF = N
+    outQR = 0
 
-    inSpects = np.fft.fft(inVoiceQ)
-    outSpects = np.fft.fft(outVoiceQ)
-
-    nowQCount = 0; #今のデータがT_fftの中の何のデータかします
+    figInQ , axInQ = plt.subplots()
+    x = np.arange(0, N , 1)
+    lineInQ, = axInQ.plot(x, np.zeros(N))
 
     def __init__(self):
-
-
-        self.inSpects = np.fft.fft(self.inVoiceQ)
-        self.OutSpects = np.fft.fft(self.inVoiceQ)
 
         self.stream = sd.Stream(
             channels=self.CHANNEL,
             samplerate=self.F_s,
-            callback=self.audioCallback
-            )
+            callback=self.audioCallback)
 
-        self.figInVoiceQ = plt.figure(0)
-        aniInVoiceQ = FuncAnimation(
-            self.figInVoiceQ,
-            self.plotInVoiceQ,
+        aniInQ = FuncAnimation(
+            self.figInVoice,
+            self.plotInVoice,
+            init_func=self.initInVoice,
             interval=self.T_fft,
-            )
-
+            blit=True)
 
         with self.stream:
             plt.show()
 
-    def plotInVoiceQ(self, frame):
+    def initInQGraph(self):
+        self.lineInQ.set_ydata([np.nan] * self.N)
+        return self.lineInQ,
+
+    def plotInQGraph(self, i):
         """ボイス波形を時間領域のグラフをプロット"""
-        while True:
-        try:
-            data = q.get_nowait()
-        except queue.Empty:
-            break
+        self.lineInQ.ste_ydata(self.inQ[self.inQR : self.inQR+self.N])
+        return self.lineInQ,
 
-    def convertVoiceToSpects(self):
+    def convertVoiceToSpects(self,convertData):
         """入ってきたマイクのデータを周波数解析して、返す"""
-        self.inSpects = np.fft.fft(self.inVoiceQ)
+        print("convert input voice to spectacles by fft\n")
+        spects = np.fft.fft(convertData)
+        return spects
 
-    def filterSpects(self):
+    def filterSpects(self,spects):
         """周波数特性を変換して、output(周波数領域)のデータ配列に入れる"""
-        self.outSpects = self.inputSpects
+        convertedSpects = spects
+        return convertedSpects
 
-    def convertSpectsToVoice(self):
+    def convertSpectsToVoice(self,convertedSpects):
         """周波数領域で変換された波形を時間領域に変換する"""
+        convertedData = 1 #TODO 周波数領域のspectaclesから時間領域の波形に変換する
+        return convertedData
 
     def audioCallback2(self):
         """T_fftごとに呼ばれるコールバック関数"""
+        print("callback by a fft\n")
+        #変換するデータの取り出し
+        convertData = self.inQ[self.inQR+1 : self.inQR+self.N]
+        self.inQR = (self.inQR + self.N) % self.Q_LEN
+        #データの変換
+#         spects = self.convertVoiceToSpects(convertData)
+#         convertedSpects = self.filterSpects(spects)
+#         convertedData = self.convertSpectsToVoice(convertedSpects)
+        #変換したデータを出力Qに追加
+#         self.outQ[self.outQF+1 : self.outQF+self.N] = convertedData[:]
+#         self.outQF = (self.outQF + self.N) % self.Q_LEN
 
-    def audioCallback(self, inVoice, outVoice, frames, time, status):
+    def audioCallback(self, indata, outdata, frames, time, status):
         """サンプリングごとに呼ばれるコールバック関数"""
         if status:
             print(status, file=sys.stderr) #error status
-
-        self.inVoiceQ.put(inVoice)
-
+        #リングQに追加
+        self.inQ[self.inQF] = indata
+        self.inQF = (self.inQF + 1) % self.Q_LEN
         #T_fftのタイミング=T_sが一定回数のタイミングで、audioCallback2を呼び出す
         if int(time % self.T_fft) == 0:
             threadConvert = threading.Thread(target=self.audioCallback2)
             threadConvert.start()
-        
-        outVoice[:] = self.outVoiceQ[self.nowQCount]
+        #リングQから取り出し
+#         outdata = self.outQ[self.outQR + 1]
+#         self.outQR = (self.outQR + 1) % self.Q_LEN
+        outdata = indata
 
 if __name__ == '__main__':
     Changer()
